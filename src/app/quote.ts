f30bb1a18d9eaafbee85bb245f2168eba1508ea6
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-09 05:05:02 +0300

    add more variables to the blue print

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,11 @@
 export class Quote {
+    upVote:number;
+    downVote:number;
+    showDetails:boolean;
 
-    constructor(public quotePoster:string, public quoteAuthor:string, public quoteName:string, public quoteDesc:string){
-        
+    constructor(public quotePoster:string, public quoteAuthor:string, public quoteName:string, public quoteDesc:string, public created:Date){
+        this.upVote = 0;
+        this.downVote = 0;
+        this.showDetails = false;
     }
 }
