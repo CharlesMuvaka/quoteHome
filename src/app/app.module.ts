fdc1ecd10c1fb968b29740a471ecfa15e24583bc
CharlesMuvaka12345 <charles.uvaka@student.moringaschool.com>
2022-04-10 22:51:54 +0300

    add formsmodule import to the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,37 @@ import { FormsModule } from '@angular/forms';
 
 
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { FormComponent } from './form/form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteAgePipe } from './quote-age.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SelectQuoteWithHighVotesDirective } from './select-quote-with-high-votes.directive';
+import { ChangeColorDirective } from './change-color.directive';
+import { DownChangeColorDirective } from './down-change-color.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     FormComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    QuoteAgePipe,
+    SelectQuoteWithHighVotesDirective,
+    ChangeColorDirective,
+    DownChangeColorDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule 
+    FormsModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule, 
+    
   
     
   ],
